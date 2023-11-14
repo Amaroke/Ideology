8b26ab3a5270baf113fb5255830c1a7d6cc313b6
Amaroke <hugo.amaroke@gmail.com>
2023-11-14 21:35:07 +0100

    Add initial project files and configurations

---
+++ backend/src/main/java/amaroke/ideology/IdeologyApplication.java
@@ -0,0 +1,13 @@
+package amaroke.ideology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeologyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/amaroke/ideology/QuantifyApplicationTests.java
@@ -0,0 +1,13 @@
+package amaroke.ideology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
